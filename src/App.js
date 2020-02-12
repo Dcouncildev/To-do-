25ca46d4e4bcb9d0a459da88ca97174ddf444987
Dcouncildev <dwightcouncil728300@gmail.com>
2020-02-12 14:37:03 -0500

    todo

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import todos from './components/Todos'
+import Todos from './components/Todos';
+function App() {
+  return (
+    <div>
+      <Todos />
+    </div>
+  ); 
+}
+
+export default App;
---
+++ src/components/Todos.js
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+const Todos = () => {
+  const [todos, setTodos] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+  const deleteTodos = indexToDelete => {
+    setTodos(prevTodos => {
+      return prevTodos.filter((value, index) => {
+        return indexToDelete !== index;
+      });
+    });
+  };
+        return (
+    <div>
+      <div className="input-group mb-3">
+          <input type="text"
+            className="form-control"
+             placeholder="Enter new todo"
+            aria-label="Enter new todo"
+            aria-describedby="button-addon2"
+             name={inputValue}
+             value={inputValue}
+             onChange={e => setInputValue(e.target.value)} />
+            <div className="input-group-append">
+             <button
+            className="btn btn-outline-secondary"
+            type="button"
+            onClick={() => {
+              setTodos(prevTodos => [...prevTodos, inputValue]);
+              setInputValue("");
+            }} >Add</button>
+     </div>
+      </div>
+      {todos.map((value, index) => {
+        console.log(value,index);
+        return (
+          <li className="list-group-item d-flex justify-content-between align-items-center"key={index}>{" "}{value}{" "}<span type="button"onClick={() => {deleteTodos(index);}} >X</span>{" "}</li>);})}
+          </div>
+          );
+        };
+
+            export default Todos;
+
+
+     
