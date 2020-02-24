b05d97a7baa0af53a37d95b1711c6d6bf3bf1498
Dcouncildev <dwightcouncil728300@gmail.com>
2020-02-24 15:25:34 -0500

    changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import todos from './components/Todos'
 import Todos from './components/Todos';
 
 function App() {
--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,8 +1,71 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import "../App.css";
+const url = "https://assets.breatheco.de/apis/fake/todos/user/Dcouncildev";
 const Todos = () => {
   const [todos, setTodos] = useState([]);
+  const [init, setInit] = useState(true);
   const [inputValue, setInputValue] = useState("");
-  const deleteTodos = indexToDelete => {
+  useEffect(() => {
+    const fetchGetTodos = () => {
+      return fetch(url)
+        .then(res => res.json())
+        .then(res => {
+          return res;
+        })
+        .catch(err => console.log("error:" + err));
+    };
+    const fetchCreateUser = () => {
+      return fetch(url, {
+        method: "POST",
+        body: JSON.stringify([]),
+        headers: { "Content-Type": "application/json" }
+      })
+        .then(res => res.json())
+        .then(res => {
+          return res;
+        })
+        .catch(err => console.log("error:" + err));
+    };
+    const fetchUpdateToDos = () => {
+      const todosData = todos.map(todo => {
+        return { label: todo, done: false };
+      });
+      return fetch(url, {
+        method: "PUT",
+        body: JSON.stringify(todosData),
+        headers: { "Content-Type": "application/json" }
+      })
+        .then(res => res.json())
+        .then(res => {
+          return res;
+        })
+        .catch(err => console.log("error:" + err));
+    };
+    //  Making GET request, testing is user exits
+    if (init === true) {
+      fetchGetTodos().then(res => {
+        console.log("response: " + JSON.stringify(res));
+        // if user does not exist, we get a "msg"
+        if (res.msg) {
+          // If user does not exists, we'll create it
+          console.log("user does not exists");
+          fetchCreateUser().then(() => {
+            fetchGetTodos(url).then(res =>
+              setTodos(res.map(todo => todo.label))
+            );
+            setInit(false);
+          });
+        } else {
+          setTodos(res.map(todo => todo.label));
+          setInit(false);
+        }
+      });
+    } else {
+      fetchUpdateToDos();
+    }
+  }, [todos, init]);
+  console.log(todos);
+  const deleteTodo = indexToDelete => {
     setTodos(prevTodos => {
       return prevTodos.filter((value, index) => {
         return indexToDelete !== index;
@@ -10,9 +73,11 @@ const Todos = () => {
     });
   };
   return (
-    <div class="bigBox">
-      <h1 class="mainTitle">To-Do List</h1>
-      <div className="mainInput input-group">
+    <div className="Todo">
+      <h1 className="Font">
+        <b>My Todo List</b>
+      </h1>
+      <div className=" Todo2 input-group">
         <input
           type="text"
           className="form-control"
@@ -25,7 +90,7 @@ const Todos = () => {
         />
         <div className="input-group-append">
           <button
-            className="btn btn-outline-secondary"
+            className="btn btn-secondary"
             type="button"
             onClick={() => {
               setTodos(prevTodos => [...prevTodos, inputValue]);
@@ -36,30 +101,30 @@ const Todos = () => {
           </button>
         </div>
       </div>
-      {todos.map((value, index) => {
-        console.log(value, index);
-        return (
-          <li
-            className="mainLi list-group-item d-flex justify-content-between align-items-center"
-            key={index}
-          >
-            {" "}
-            {value}{" "}
-            <span
-              class="buttonDelete btn btn-danger"
-              type="button"
-              onClick={() => {
-                deleteTodos(index);
-              }}
+      <ul className="list-group">
+        {todos.map((value, index) => {
+          return (
+            <li
+              className="list-group-item list-group-item-secondary d-flex justify-content-between align-items-center"
+              key={index}
             >
-               X
-            </span>{" "}
-          </li>
-        );
-      })}
-      <li className="mainLi list-group-item d-flex justify-content-between align-items-center">{todos.length} Items Left</li>
+              {value}
+              <span
+                type="button"
+                onClick={() => {
+                  deleteTodo(index);
+                }}
+              >
+                X
+              </span>
+            </li>
+          );
+        })}
+        <li className="list-group-item list-group-item-secondary d-flex justify-content-between align-items-center">
+          {todos.length} items left
+        </li>
+      </ul>
     </div>
   );
 };
-
 export default Todos;
